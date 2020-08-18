1814920dfab69c0074d2ee5ef31aff7fd77ce5d6
stayupchampion <vincentlinshen@iCloud.com>
2020-08-18 23:11:43 +0800

    chore(targets, main.m, ViewController.h&.m, storyboard): init environment
    - 设置 targets-SshfsTool-general-application category 为 utilities
    	表示应用类型为实用工具
    - 在 info 添加了 "application is agent" 选项并设置为 "yes"
    	表示代理运行，应用不需要窗口，不需要 dock 图标
    - 删除了初始的 ViewController 文件
    - 在 storyboard 中，删除了除了 main menu 外的所有控制器
    - main.m
    	修改代理，程序启动时直接指向 AppDelegate

--- SshfsTool/main.m
+++ SshfsTool/main.m
@@ -8,6 +8,14 @@
 
 #import <Cocoa/Cocoa.h>
 
+#import "AppDelegate.h"
+
 int main(int argc, const char * argv[]) {
+    
+    // 设置代理为AppDelegate
+    NSApplication *app = [NSApplication sharedApplication];
+    id delegate = [[AppDelegate alloc] init];
+    app.delegate = delegate;
+    
     return NSApplicationMain(argc, argv);
 }
