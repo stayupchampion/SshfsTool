a47410faffed44be6bd47f7f58cea62d28398187
stayupchampion <vincentlinshen@iCloud.com>
2020-08-19 13:43:42 +0800

    feat(AppDelegate, stb): 增加连接和断开功能
    - 连接功能
    	入口：菜单栏的 连接 项
    	作用：读取用户设置的登录信息，使用 sshfs 命令挂载远程目录到本地指定目录
    - 断开功能
    	入口：菜单栏的 断开 项
    	作用：读取用户设置的本地挂载路径，卸载远程目录
    - 关闭沙盒模式
    	修改设置：targets-sshfstool-capabilities-app sandbox 设置为OFF
    	作用：关闭应用程序的沙盒模式，使 shell 命令可以访问系统命令

--- SshfsTool/AppDelegate.m
+++ SshfsTool/AppDelegate.m
@@ -32,6 +32,49 @@
     // Insert code here to tear down your application
 }
 
+// 执行shell字符串
+- (void)execute_shell_task:(NSString *)shell_str {
+    // 创建shell任务
+    NSTask *shellTask = [[NSTask alloc]init];
+    [shellTask setLaunchPath:@"/bin/bash"];
+    
+    // -c 表示将后面的内容当成shellcode来执行
+    [shellTask setArguments:[NSArray arrayWithObjects:@"-c", shell_str, nil]];
+    
+    //    NSPipe *pipe = [[NSPipe alloc]init];
+    //    [shellTask setStandardOutput:pipe];
+    
+    // 执行shell任务
+    [shellTask launch];
+    // 阻塞进程
+    //    [shellTask waitUntilExit];
+    
+    //    NSFileHandle *file = [pipe fileHandleForReading];
+    //    NSData *data = [file readDataToEndOfFile];
+    //    NSString *strReturnFromShell = [[NSString alloc]initWithData:data encoding:NSUTF8StringEncoding];
+    //    NSLog(@"The return content:\n%@",strReturnFromShell);
+}
+
+// 连接
+- (IBAction)item1:(id)sender {
+    // 读取ssh登录信息
+    NSDictionary *login_dict = [[NSUserDefaults standardUserDefaults] dictionaryForKey:@"user0"];
+    // 拼接登陆shell命令
+    NSString *shell_str = [NSString stringWithFormat:@"/usr/local/bin/sshfs %@@%@:%@ %@ -C -o reconnect", login_dict[@"UserName"], login_dict[@"Host"], login_dict[@"RemotePath"], login_dict[@"LocalPath"]];
+    // 执行shell任务: sshfs connect
+    [self execute_shell_task:shell_str];
+}
+
+// 断开
+- (IBAction)disconnect:(id)sender {
+    // 读取登录信息
+    NSDictionary *login_dict = [[NSUserDefaults standardUserDefaults] dictionaryForKey:@"user0"];
+    // 拼接卸载命令
+    NSString *shell_str = [NSString stringWithFormat:@"diskutil unmount %@", login_dict[@"LocalPath"]];
+    // shell: sshfs disconnect
+    [self execute_shell_task:shell_str];
+}
+
 - (IBAction)quit:(id)sender {
     [[NSApplication sharedApplication] terminate:self];
 }
